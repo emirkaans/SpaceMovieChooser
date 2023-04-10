579a620a2552c8612e116b8c303e75c7b8e55b12
Emir Kaan <113292350+emirkaans@users.noreply.github.com>
2023-04-10 18:37:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 "use strict";
 
 const chooseButton = document.querySelector(".btn--1");
-const watchButton = document.querySelector(".btn--2");
 const movieName = document.querySelector(".movie--name");
+const imdb = document.querySelector(".movie--link");
 
-const movies = [
-  "Benim Güneşli Maad'ım",
-  "The Giver",
-  "Legion",
-  "Inland Empire",
-  "Frank",
-  "The Color of Pomegranates",
-  "Paprika",
-  "Kika",
-  "Marnie Oradayken",
-  "The Fifth Element",
-  "Beden ve Ruh",
-  "The Girl With All The Gifts",
-  "The Man Who Fell Earth",
-];
+const chooseFunc = async function () {
+  const movieId = Math.trunc(Math.random() * 999999 + 1);
+  let movieText;
 
-const chooser = function () {
-  const movieNum = Math.trunc(Math.random() * movies.length);
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=c733c91889e635bc56cd8f804e5afd37`
+    );
 
-  movieName.textContent = movies[movieNum];
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data);
+
+      if (data.adult) return;
+
+      movieText = `${data.title} (${data.release_date?.slice(0, 4)})
+      `;
+
+      const movieLink = data.imdb_id
+        ? `https://www.imdb.com/title/${data.imdb_id}`
+        : "https://www.imdb.com/";
+
+      movieName.textContent = movieText;
+      imdb.href = movieLink;
+      imdb.classList.add("active");
+    }
+
+    if (!res.ok) {
+      movieName.textContent = "Signal loss! Please try again!";
+      imdb.classList.remove("active");
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-chooseButton.addEventListener("click", chooser);
+chooseButton.addEventListener("click", chooseFunc);
